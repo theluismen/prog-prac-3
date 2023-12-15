249861051463863a21bfa568efe28c4ce46d4b81
pau-valls <153213728+pau-valls@users.noreply.github.com>
2023-12-15 18:59:54 +0100

    Update UsaActivitat.java

--- src/Aplicacio/UsaActivitat.java
+++ src/Aplicacio/UsaActivitat.java
@@ -30,4 +30,33 @@ class UsaActivitat {
 
         System.out.println( taller.toString() );
     }
+
+
+        Activitat a = new Activitat("Carreres", "Tarragona", 43400, 13, new Entitat("Redbull", "123456789", "redbull@gmail.com"));
+        Xerrada x = new Xerrada("Elon Musk", "Tecnologia", "Reus", 43500, 23, new Entitat("SpaceX", "98765431", "spacex@gmail.com"));
+        Visita v = new Visita(true,true, "Fabricacio", "lleida", 43700, 18, new Entitat("Mahle", "456123789", "mahle@gmail.com"));
+
+        System.out.println(a.toString());
+        System.out.println(x.toString());
+        System.out.println(v.toString());
+
+
+        // Validar Setters
+        a.setCPostal(66666);
+        x.setNomAutor("Melchor");
+        v.setAudioguia(false);
+        System.out.println(a.toString());
+        System.out.println(x);
+        System.out.println(v);
+
+        // Validar Getters
+        System.out.println("Nom: "+ a.getNom());
+        System.out.println("Nom: "+ x.getNom());
+        System.out.println("Nom: "+ v.getNom());
+
+        System.out.println("Codi: "+ x.getCodi());
+        System.out.println("Codi: "+ v.getCodi());
+        System.out.println("Codi: "+ a.getCodi());
+    }
+    
 }
