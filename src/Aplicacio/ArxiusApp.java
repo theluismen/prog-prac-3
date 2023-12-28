ca4681ebb2484f757ffafe1a18b38be0136daabd
pau-valls <153213728+pau-valls@users.noreply.github.com>
2023-12-28 19:37:12 +0100

    Update ArxiusApp.java

--- src/Aplicacio/ArxiusApp.java
+++ src/Aplicacio/ArxiusApp.java
@@ -5,5 +5,5 @@ public class ArxiusApp {
     public static final String ARXIU_ENTITATS   = new String("./data/Entitats.txt");
     public static final String ARXIU_ACTIVITATS = new String("");
     public static final String ARXIU_USUARIS    = new String("./data/Usuaris.txt");
-    public static final String ARXIU_RESERVES   = new String("");
+    public static final String ARXIU_RESERVES   = new String("./data/Reserves.txt");
 }
